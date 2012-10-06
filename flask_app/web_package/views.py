84cc1a597f09dee0a1ee00cffba21b628821db91
Dalton Hubble <dghubble@gmail.com>
2012-10-06 02:38:53 -0400

    Create routes for User resources

--- flask_app/web_package/views.py
+++ flask_app/web_package/views.py
@@ -1,10 +1,37 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from web_package import app
 
 @app.route('/')
-def hello_world():
+def index():
 	return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-	return 'hello, world'
+# User Resources
+#######################################################
+@app.route('/user/<id>', methods = ['GET'])
+def user_profile(id):
+	print "Your identifier is " + str(id)
+	return render_template('user_profile.html')
+
+
+@app.route('/user/<id>/edit', methods = ['GET'])
+def user_edit(id):
+	print "User ", str(id), " attempting to edit"
+	return render_template('user_edit.html')
+
+
+@app.route('/user/new', methods = ['GET'])
+def user_signup():
+	return render_template('user_signup.html')
+
+
+@app.route('/user/create', methods = ['POST'])
+def user_create():
+	#Create a new user
+	print "TODO: Create a user"
+	return redirect(url_for('index'))
+
+
+# Post Resources
+#######################################################
+
+
