e0d9a24568cf18b600260f10ba685f21b907ca3b
Thomas Garver <tgarv@mit.edu>
2012-10-06 17:15:36 -0400

    Removed latitude/longitude dependencies from User class

--- flask_app/schema.sql
+++ flask_app/schema.sql
@@ -2,7 +2,5 @@ drop table if exists users;
 create table users(
 	username string primary key,
 	hash string not null,
-	salt string not null,
-	latitude integer,
-	longitude integer
+	salt string not null
 )
--- flask_app/web_package/user.py
+++ flask_app/web_package/user.py
@@ -3,11 +3,10 @@ from web_package import connect_db
 
 
 class User(object):
-	def __init__(self, username, password_hash, salt, latitude=None, longitude=None):
+	def __init__(self, username, password_hash, salt):
 		self.username = username
 		self.password_hash = password_hash
 		self.salt = salt
-		self.set_location(latitude, longitude)
 
 	@staticmethod
 	def create(username, password):
@@ -16,7 +15,7 @@ class User(object):
 		necessarily used for creating a new User in the database, so they're separated into different functions).
 		"""
 		password_hash, salt = hash_password(password)
-		u = User(username, password_hash, salt, None, None)
+		u = User(username, password_hash, salt)
 		u.save()
 		return u
 
@@ -26,16 +25,6 @@ class User(object):
 		"""
 		self.password_hash, self.salt = hash_password(password)
 
-	def set_location(self, latitude, longitude):
-		"""
-		Set the user's location, latitude and longitude are ints.
-		"""
-		self.latitude = latitude
-		self.longitude = longitude
-
-	def get_location(self):
-		return (self.latitude, self.longitude)
-
 	def save(self):
 		"""
 		Wrapper around the database upsert for the User. You need to call save if you want to insert the 
@@ -43,11 +32,10 @@ class User(object):
 		"""
 		object_dict = self.__dict__
 		print object_dict
-		query = "insert or replace into users (username, hash, salt, latitude, longitude) values (?, ?, ?, ?, ?)"
+		query = "insert or replace into users (username, hash, salt) values (?, ?, ?)"
 
 		db = connect_db()
-		db.execute(query, [object_dict['username'], object_dict['password_hash'], object_dict['salt'], 
-						   object_dict['latitude'], object_dict['longitude']])
+		db.execute(query, [object_dict['username'], object_dict['password_hash'], object_dict['salt']])
 		db.commit()
 		db.close()
 
