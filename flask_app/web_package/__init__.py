4476b13d72d07235fd9d9556701cf1aebc3e5acf
Dalton Hubble <dghubble@gmail.com>
2012-10-06 18:25:51 -0400

    Setup database and imports Pair programming session 2

--- flask_app/web_package/__init__.py
+++ flask_app/web_package/__init__.py
@@ -1,22 +1,24 @@
-from flask import Flask
+from flask import Flask, g
 from contextlib import closing
 import sqlite3
+#import sqlite3
 
 #App configuration will go here.
-DATABASE = "db/code_blue.db"
+#DATABASE = "db/code_blue.db"
 app = Flask(__name__)
-app.config.from_object(__name__)
-
+app.config.from_object('web_package.config')
 
 def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'])
 
+@app.before_request
+def before_request():
+    g.db = connect_db()
 
-def init_db():
-	with closing(connect_db()) as db:
-		with open('schema.sql') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
 
 # To prevent circular dependency problems, import views at the end and do not use the views inside __init__.py.
-from web_package import views
+from web_package import routes
