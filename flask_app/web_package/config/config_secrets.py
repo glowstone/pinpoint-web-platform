53af6c286e9885486671615afe9e881a81177bde
Dalton Hubble <dghubble@gmail.com>
2012-10-15 14:55:04 -0400

    Separate secret configuration which can be isolated

---
+++ flask_app/web_package/config/config_secrets.py
@@ -0,0 +1,8 @@
+
+
+
+username = "tgarv"
+password = "lam63sot"
+db_name = "tgarv+code_blue"
+
+
--- flask_app/web_package/config/dev_config.py
+++ flask_app/web_package/config/dev_config.py
@@ -1,5 +1,6 @@
 # Development Application Configuration
 import os
+from config_secrets import username, password, db_name
 
 # dialect = "sqlite:////"
 # db_name = "development.sqlite"
@@ -7,11 +8,13 @@ import os
 # SQLALCHEMY_DATABASE_URI = dialect + os.path.join(os.path.dirname(os.path.dirname(__file__)), "db", db_name)
 
 dialect = "mysql://"
-username = "tgarv"
-password = "lam63sot"	# This is a random password only used for sql.mit.edu, should probably store it somewhere else
+# Username and password to be moved to a secret file. DB will have independent user management
+#username = "tgarv"
+#password = "lam63sot"	# This is a random password only used for sql.mit.edu, should probably store it somewhere else
+# Eventually databse and host will be shared and centralized.
 host = "sql.mit.edu"
-db_name = "tgarv+code_blue"
-
+#db_name = "tgarv+code_blue"           #Add this back once we just have one db
+ 
 SQLALCHEMY_DATABASE_URI = dialect + username + ":" + password + "@" + host + "/" + db_name
 print "SQLALCHEMY_DATABASE_URI: %s" % SQLALCHEMY_DATABASE_URI
 
