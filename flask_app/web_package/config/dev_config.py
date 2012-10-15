4e14985ea044fd25746fe77a5b1f199da8e5c9b7
Thomas Garver <tgarv@mit.edu>
2012-10-15 13:36:37 -0400

    Set up MySQL database in config. You might need to install something like "sudo apt-get install libmysqlclient-dev" to get it to work.

--- flask_app/web_package/config/dev_config.py
+++ flask_app/web_package/config/dev_config.py
@@ -1,10 +1,19 @@
 # Development Application Configuration
 import os
 
-dialect = "sqlite:////"
-db_name = "development.sqlite"
+# dialect = "sqlite:////"
+# db_name = "development.sqlite"
 
-SQLALCHEMY_DATABASE_URI = dialect + os.path.join(os.path.dirname(os.path.dirname(__file__)), "db", db_name)
+# SQLALCHEMY_DATABASE_URI = dialect + os.path.join(os.path.dirname(os.path.dirname(__file__)), "db", db_name)
+
+dialect = "mysql://"
+username = "tgarv"
+password = "lam63sot"	# This is a random password only used for sql.mit.edu, should probably store it somewhere else
+host = "sql.mit.edu"
+db_name = "tgarv+code_blue"
+
+SQLALCHEMY_DATABASE_URI = dialect + username + ":" + password + "@" + host + "/" + db_name
+print "SQLALCHEMY_DATABASE_URI: %s" % SQLALCHEMY_DATABASE_URI
 
 # Eventually move this to a separate file
 SECRET_KEY = 'development key'
