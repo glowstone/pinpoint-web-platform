cdb2fd9dfc390873aea6ff2d433104f936c708ea
Dalton Hubble <dghubble@gmail.com>
2013-02-12 02:28:25 -0500

    Now viewing your own profile (the question listing page) or that of another user shows the correct set of questions and renders all the pins on the map with multiple overlays.

--- app_pkg/static/js/question_list.js
+++ app_pkg/static/js/question_list.js
@@ -64,7 +64,8 @@ require([
 			///////////////////////////////////////////////////////////////////
 			if (YOUR_PAGE) {
 
-				question_collection = new QuestionCollection()
+				question_collection = new QuestionCollection();
+				question_collection_aux = new QuestionCollection();
 				question_creator = new QuestionCollectionCreatorView({
 					el: $("#question-creator-region"),
 					collection: question_collection,
@@ -72,42 +73,84 @@ require([
 				});
 				question_creator.render();
 
-				// question_display = new QuestionCollectionDisplayView({
-				// 	el: $("#global-question-display-region"),
-				// 	collection: question_collection,
-				// 	fetch_data: {},
-				// 	generate_question_view: generate_question_view,
-				// 	empty_message: "No Questions yet. You could be the first to ask one."
-				// });
-				// question_display.render()
-				// console.log(USER_ID)
-
-				question_display = new QuestionCollectionDisplayView({
+				user_question_display = new QuestionCollectionDisplayView({
 					el: $("#user-question-display-region"),
 					collection: question_collection,
 					fetch_data: {
-						// q: JSON.stringify({
-						// "filters": [
-						// 		{
-						// 			name: "author__id",
-						// 			op: "==",
-						// 			val: 2,
-						// 		},
-						// 	]
-						// }),
+						q: JSON.stringify({
+							"filters": [
+								{
+									name: "user_id",
+									op: "==",
+									val: USER_ID,
+								},
+							]
+						}),
 					},
 					generate_question_view: generate_question_view,
 					empty_message: "You haven't created any Questions. Try pinning one on the map now?"
 				});
-				question_display.render()
+				user_question_display.render();
+
+				global_question_display = new QuestionCollectionDisplayView({
+					el: $("#global-question-display-region"),
+					collection: question_collection_aux,
+					fetch_data: {
+						q: JSON.stringify({
+							"filters": [
+								{
+									name: "user_id",
+									op: "!=",
+									val: USER_ID,
+								},
+							]
+						}),
+					},
+					generate_question_view: generate_question_view,
+					empty_message: "No Questions yet. You could be the first to ask one."
+				});
+				global_question_display.render()
 
 				question_on_map = new QuestionCollectionMapView({
 					el: $(map),
 					collection: question_collection,
 					map_reference: map,				
 				});
+
+				question_on_map_aux = new QuestionCollectionMapView({
+					el: $(map),
+					collection: question_collection_aux,
+					map_reference: map,				
+				});
+
 			}
 			else {
+				question_collection = new QuestionCollection();
+
+				page_owner_question_display = new QuestionCollectionDisplayView({
+					el: $("#page-owner-question-display-region"),
+					collection: question_collection,
+					fetch_data: {
+						q: JSON.stringify({
+							"filters": [
+								{
+									name: "user_id",
+									op: "==",
+									val: PAGE_OWNER_ID,
+								},
+							]
+						}),
+					},
+					generate_question_view: generate_question_view,
+					empty_message: "This user has not yet created any Questions. :("
+				});
+				page_owner_question_display.render();
+
+				question_on_map_aux = new QuestionCollectionMapView({
+					el: $(map),
+					collection: question_collection,
+					map_reference: map,				
+				});
 
 
 			}
